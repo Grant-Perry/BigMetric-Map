9b084dc0490b41bd358ee720cc9a76b91ef06de9
Grant Perry <121777018+Grant-Perry@users.noreply.github.com>
2024-01-13 13:12:41 -0500

    Initial Commit

---
+++ BigMetric-Map/BigMetric_MapApp.swift
@@ -0,0 +1,19 @@
+//   BigMetric_MapApp.swift
+//   BigMetric-Map
+//
+//   Created by: Grant Perry on 1/13/24 at 1:12 PM
+//     Modified: 
+//
+//  Copyright © 2024 Delicious Studios, LLC. - Grant Perry
+//
+
+import SwiftUI
+
+@main
+struct BigMetric_MapApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ BigMetric-Map/ContentView.swift
@@ -0,0 +1,26 @@
+//   ContentView.swift
+//   BigMetric-Map
+//
+//   Created by: Grant Perry on 1/13/24 at 1:12 PM
+//     Modified: 
+//
+//  Copyright © 2024 Delicious Studios, LLC. - Grant Perry
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
